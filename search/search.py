cbc63ce4e38b01a9d70c5af12265e17ecb459706
jrsmith0 <johnrsmith20003@gmail.comqsfuckgit config --global user.email johnrsmith20003@gmail.com>
2024-09-09 17:39:47 -0500

    2 close, 3,4,5

--- search/search.py
+++ search/search.py
@@ -82,19 +82,15 @@ def depthFirstSearch(problem):
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
     """
-
-
     def dfsHelp(start_state, problem, goal_reached) :
 
         if goal_reached[0] == 1 :
             print("return from if statement")
             return stack
-        
         if (problem.isGoalState(start_state)) :
             print("Goal reached")
             goal_reached[0] = 1
             return stack
-        
         visited.add(start_state)
         # actions.add(start_state.action)
 
@@ -126,50 +122,67 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+
+    # def ucs_init(cur) :
+    #     if cur in logged :
+    #         return None
+    #     print("cur: ", cur, "; is goal? ", problem.isGoalState(cur))
+    #     child = problem.getSuccessors(cur)
+    #     if not child or cur in logged :
+    #         return None
+    #     logged.append(cur)
+        
+    #     for c in child :
+    #         ucs_init(c[0])
+    
+    # logged = list()
+
+    # cur = problem.getStartState()
+    # ucs_init(cur)
+    # print(logged)
+
     visited = list()
     queue = util.Queue()
-    actions = list()
+    bfs_res = list()
 
     start = problem.getStartState()
-    visited.append(start)
     queue.push(start)
-    previous = None
-    while not queue.isEmpty() :
+    goal_pending = True
+    while not queue.isEmpty() and goal_pending:
         cur = queue.pop()
-        print("cura: ",cur)
+        print("examine: ",cur)
         if problem.isGoalState(cur) :
+            goal_pending = False
             print("Goal reached")
             break
-        children = problem.getSuccessors(cur) 
-        for neighbor in children :
-            print("neighbor: ", neighbor)
-            if neighbor[0] not in visited :
-                print("prev: ", previous)
-                print("cur: ", cur)
-
-                
-                if previous and neighbor[0] in problem.getSuccessors(previous): 
-                    visited.append(neighbor[0])
+        if cur not in visited : 
+            visited.append(cur)
+          
+            children = problem.getSuccessors(cur) 
+            for neighbor in children :
+                if neighbor[0] not in visited :
+                    print("     expand: ", neighbor[0])
+                    if problem.isGoalState(neighbor[0]) :
+                        goal_pending = False
+                        print("Goal reached")
+                        break
                     queue.push(neighbor[0])
-                    actions.append(neighbor)
-                    previous = cur
-                elif not previous : 
-                    visited.append(neighbor[0])
-                    queue.push(neighbor[0])
-                    actions.append(neighbor)
-                    previous = cur
-    ractions = actions[::-1]
+                    bfs_res.append(neighbor)
 
-    # prev = ractions[0]
-    # for action in ractions :
-    #     if (action != prev) :
-    #         childrens = problem.getSuccessors(action[0])
-    #         print("prev: ", prev)
-    #         if prev not in childrens :
-    #             print("Remove action", action)
-    #             ractions.remove(action)
-    #         else :
-    #             prev = action
+    rev_bfs_res = bfs_res[::-1]
+
+    prev = rev_bfs_res[0]
+    actions = list()
+    # actions.append(prev)
+    # print(rev_bfs_res)
+    for action in rev_bfs_res :
+        if (action != prev) :
+            childrens = problem.getSuccessors(action[0])
+            # print("prev: ", prev)
+            # print("     action: ", action)
+            if prev in childrens :
+                prev = action
+                actions.append(action)
 
     print(actions[::-1])
     retlist = []
@@ -177,45 +190,72 @@ def breadthFirstSearch(problem):
         retlist.append(a[1])
     print(retlist[::-1])
     return retlist[::-1]
-    return actions[::-1]
 
  
 # Dijkstra's algorithm
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    def ucs_init(cur) :
-        if cur in logged :
-            return None
-        
-        child = problem.getSuccessors(cur)
-        if not child or cur in logged :
-            return None
-        logged.append(cur)
-        
-        for c in child :
-            dij_queue.push(c, 10000) 
-            ucs_init(c[0])
+    # def ucs_init(cur) :
 
-    dij_queue = util.PriorityQueue()
-    visited = list()
-    logged = list()
-
-    cur = problem.getStartState()
-    ucs_init(cur)
+    #     if cur in logged :
+    #         return None
         
-    prev = None
-    while not dij_queue.isEmpty() :
-        cur = dij_queue.pop()
-        print("CUR: ", cur)
-        if problem.isGoalState(cur[0]) :
-            print("Goal reached")
-            break
-        children = problem.getSuccessors(cur[0]) 
-        for neighbor in children :
-            dij_queue.update(neighbor, priority(cur) + cost(neighbor))
+    #     child = problem.getSuccessors(cur)
+    #     if not child or cur in logged :
+    #         return None
+    #     logged.append(cur)
+        
+    #     for c in child :
+    #         working_queue.push(c[0], float('inf')) 
+    #         ucs_table[c[0]] = float('inf')
+    #         ucs_init(c[0])
 
-    return logged
+    # working_queue = util.PriorityQueue()
+
+    # visited = set()
+
+    # cur = problem.getStartState()
+    # ucs_init(cur)
+    # ucs_table[cur] = 0
+    # working_queue.update(cur, 0)
+    # print(ucs_table)
+        
+    # while not working_queue.isEmpty() :
+    #     cur = working_queue.pop()
+    #     print("CUR: ", cur)
+    #     if problem.isGoalState(cur) :
+    #         print("Goal reached")
+
+    #         break
+    #     children = problem.getSuccessors(cur) 
+    #     if cur not in visited :
+    #         visited.add(cur)
+    #         for neighbor in children :
+    #             print(neighbor)
+    #             new_cost = ucs_table[cur] + ucs_table[neighbor[0]]
+    #             print(new_cost)
+    #             if (ucs_table[neighbor[0]][1] < new_cost) :
+    #                 working_queue.update(neighbor, new_cost)
+    #                 ucs_table.update(neighbor, new_cost)
+    # for key in ucs_table :
+    #     print (ucs_table[key])
+    # return logged
+    frontier = util.PriorityQueue()
+    frontier.push((problem.getStartState(), [], 0),0)
+    visited = set()
+
+    while not frontier.isEmpty():
+        state, actions, cost = frontier.pop()
+
+        if problem.isGoalState(state):
+            return actions
+        
+        if state not in visited:
+            visited.add(state)
+            for successor, action, stepCost in problem.getSuccessors(state):
+                frontier.push((successor, actions + [action], cost + stepCost), cost + stepCost)
+    return []
             
 
 def nullHeuristic(state, problem=None):
@@ -228,7 +268,21 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    frontier = util.PriorityQueue()
+    frontier.push((problem.getStartState(), [], 0),0)
+    visited = set()
+
+    while not frontier.isEmpty():
+        state, actions, cost = frontier.pop()
+
+        if problem.isGoalState(state):
+            return actions
+        
+        if state not in visited:
+            visited.add(state)
+            for successor, action, stepCost in problem.getSuccessors(state):
+                frontier.push((successor, actions + [action], cost + stepCost), cost + stepCost + heuristic(successor, problem))
+    return []
 
 
 # Abbreviations
